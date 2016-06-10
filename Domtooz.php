62a171230dae1734a7ffe3bb32c358c3de4da67c
Travis van der Font <travis.font@gmail.com>
2016-06-10 15:54:53 +0200

    add whois functionality

--- Domtooz.php
+++ Domtooz.php
@@ -35,7 +35,86 @@ class Domtooz
             return FALSE;
         }
     }
+
+    // Whois sever lookup function
+    public static function queryWhois($domain, $registrar = NULL)
+    {
+        // fixing the domain name format
+        $domain = strtolower(trim($domain));
+        $domain = preg_replace("/^http:\/\//i", '', $domain);
+        $domain = preg_replace("/^www\./i", '', $domain);
+        $domain = explode('/', $domain);
+        $domain = trim($domain[0]);
+
+        // server output string
+        $output = FALSE;
+
+        if (empty($registrar))
+        {
+            $servers = array();
+
+            // retrieving server resoures and lists
+            foreach (iniParser(self::$domain_file) as $tld => $entity)
+            {
+                if (!empty($entity))
+                {
+                    list($server,) = explode('|', $entity, 2);
+
+                    // if there are multiple TLD servers (pick the first one)
+                    if (strpos($server, ',') !== FALSE)
+                    {
+                        $server = explode(',', $server);
+                        $server = trim($server[0]);
+                    }
+
+                    $servers[strtolower($tld)] = strtolower($server);
+                }
+            }
+
+            // split the TLD from domain name
+            $_domain = explode('.', $domain);
+            $lst     = count($_domain)-1;
+            $ext     = $_domain[$lst];
+
+            if (!isset($servers[$ext]))
+            {
+                trigger_error('No matching nic server found!', E_USER_ERROR);
+                // http://www.nirsoft.net/whois-servers.txt
+            }
+
+            $nic_server = $servers[$ext];
+        }
+        else
+        {
+            $nic_server = strip_tags(trim($registrar));
+        }
+
+        // connect to whois server
+        if ($conn = fsockopen($nic_server, 43))
+        {
+            fputs($conn, $domain."\r\n");
+
+            while (!feof($conn))
+            {
+                $output .= fgets($conn, 128);
+            }
+            fclose($conn);
+        }
+        else
+        {
+            trigger_error('Could not connect to '.$nic_server.'!', E_USER_ERROR);
+        }
+
+        return $output;
+    }
+
+    // findWhois($string) {}
 }
 
+if ($result = Domtooz::queryWhois('travisfont.com'))
+{
+    echo '<pre>';
+    print_r($result);
+}
 
-var_dump(Domtooz::checkTLD(Domtooz::explodeTLD('111-222-1933email@address.com')));
+#var_dump(Domtooz::checkTLD(Domtooz::explodeTLD('111-222-1933email@address.com')));
